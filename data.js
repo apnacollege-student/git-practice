4b36134da93adb0daeb787de32f3df63989acbeb
apnacollege-student <yuvashree@users.noreply.github.com>
2026-03-15 12:57:48 +0530

    Updated data.js

--- data.js
+++ data.js
@@ -1,2 +1,21 @@
 console.log("Learning git today");
 
+const Doctors = [
+    {
+        id:1,
+        name:"Isha Sharma",
+        specilization:"Dermatologist",
+        ConsultationFee: 500,
+        Timings:"Mon-Fri" 
+    },
+
+    {
+        id: 2,
+        name:"Riya Mehta",
+        specilization:"Dermatologist",
+        ConsultationFee: 800,
+        Timings:"Mon-Fri" 
+    },
+]
+
+export default Doctors;
