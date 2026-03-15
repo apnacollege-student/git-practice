d63b9711181869d9ba7f204d6a8c969da79dc2d1
apnacollege-student <yuvashree@users.noreply.github.com>
2026-03-15 13:11:50 +0530

    Added new file

---
+++ drivers.js
@@ -0,0 +1,23 @@
+const drivers=[
+    {
+        id:1,
+        name:Ravi,
+        age: 22,
+    },
+    {
+        id:2,
+        name:Nishi,
+        age: 22,
+    },
+    {
+        id:3,
+        name:Ishu,
+        age: 22,
+    },
+    {
+        id:4,
+        name:Mishu,
+        age: 22,
+    }
+
+]
